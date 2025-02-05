084b42aefb3ddc2b7e4a3f6af2ae82c234b563af
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-02-06 00:20:46 +0530

    practic c

--- 2number_sum.c
+++ 2number_sum.c
@@ -1,8 +1,11 @@
-# include<stdio.h>
+/*Hello World*/
+#include<stdio.h>
 int main()
 {
     int a,b,c;
-    a = 10; 
-    b = 20; 
-    c = 
+    a = 10;
+    b = 20;
+    c = a + b;
+    printf("a = %d b = %d sum = %d", a,b,c);
+    return 0;
 }
---
+++ 3number_sum.c
@@ -0,0 +1,12 @@
+/*Hello World*/
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    a = 10;
+    b = 20;
+    c = 30;
+    d = a + b + c;
+    printf("a = %d b = %d c = %d sum = %d", a,b,c,d);
+    return 0;
+}
