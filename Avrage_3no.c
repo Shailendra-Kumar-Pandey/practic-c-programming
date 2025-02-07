6bc2e8e969125451c2f2b7908737bbf5631a4e18
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-02-07 11:56:19 +0530

    practic C

---
+++ Avrage_3no.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float a , b, c, avg;
+    a = 70.0;
+    b = 40.0;
+    c = 15.0;
+    avg = (a + b + c)/3;
+    printf("a = %f b = %f c = %f avrage = %f", a, b, c, avg);
+    return 0;
+}
---
+++ Volume_of_box.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float lenght, width, height, volume_of_box;
+    lenght = 20.0;
+    width = 25.5;
+    height = 15.0;
+    volume_of_box = lenght * width * height;
+    printf("\n Volume of Box = %f", volume_of_box);
+    return 0;
+}
