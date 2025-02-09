fa0001610eabf4ad10a9f85173f8ba7ab1279ddd
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-02-09 21:55:25 +0530

    practic C

---
+++ scanf()/ractangle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    float lenght, width, area, perimeter;
+    printf("\n Enter the value of Lenght :");
+    scanf("%f",&lenght);
+    printf("\n Enter the value of width :");
+    scanf("%f",&width);
+    area = lenght * width;
+    perimeter = 2 * (lenght * width);
+    printf("\n Area of Ractangle : = %f",area);
+    printf("\n Perimeter of Ractangle : = %f",perimeter);
+    return 0;
+}
