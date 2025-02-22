4b1f509272117476daa620c53febf6091c9a0380
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-02-22 23:25:40 +0530

    Practic C

--- If else statement/Simple_Interast.c
+++ If else statement/Simple_Interast.c
@@ -1,3 +1,8 @@
+/* Write to the Program calculate and print simple interest.
+ Input Principal ammount and duration. Rate of interest 
+ depends on Principal.
+ If P <= 8000 then R = 10%
+ If P > 8000 then R = 15% */
 #include<stdio.h>
 int main()
 {
