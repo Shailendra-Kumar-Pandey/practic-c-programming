2f2764a5333f3fcc38be8e5bb3baa6989c5f8358
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-02-22 23:32:52 +0530

    Practic C

---
+++ If else statement/Simple Interest 2.c
@@ -0,0 +1,6 @@
+/* Write to the Program calculate and print simple interest.
+ Input Principal ammount and duration. Rate of interest 
+ depends on Principal.
+ If P <= 8000 then R = 10%
+ If P > 8000 and <= 15000 then R = 15% 
+ If P > 15000 then R = 18% */
