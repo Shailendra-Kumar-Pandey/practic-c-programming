af7c4c74a248822fc1e06ebc62b9fb1c1b35240b
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-02-23 23:41:38 +0530

    Practic C

--- If else statement/Simple Interest 2.c
+++ If else statement/Simple Interest 2.c
@@ -4,3 +4,22 @@
  If P <= 8000 then R = 10%
  If P > 8000 and <= 15000 then R = 15% 
  If P > 15000 then R = 18% */
+ #include<stdio.h>
+ int main()
+ {
+    float principal, time, rate, simple_interest;
+    printf("\n Enter the value of Principal : ");
+    scanf("%f", &principal);
+    printf("\n Enter the value of Time & Duration : ");
+    scanf("%f", &time);
+    if(principal <= 8000)
+        rate = 10;
+    else
+        if(principal > 8000 && principal <= 15000)
+            rate = 15;
+        else
+            rate = 18;
+    simple_interest = principal * time * rate / 100;
+    printf("\n Simple Interest = %f ", simple_interest);
+    return 0;
+ }
