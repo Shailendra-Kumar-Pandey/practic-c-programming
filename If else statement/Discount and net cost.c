a059d13cb99e946bf34c77526b8df46d342448df
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-02-24 13:54:52 +0530

    practic C

--- If else statement/Discount and net cost.c
+++ If else statement/Discount and net cost.c
@@ -1,9 +1,8 @@
-/* Write to the Program calculate and print discount and net price.
-Input cost of item. Rate of discount depends on cost.
+/* Write to the Program calculate and print discount and net price.Input cost of item. Rate of discount depends on cost.
  If C <= 5000 then R = 10%
- If C > 5000 and <= 20000 then R = 20% 
- If C > 20000 and <= 30000 then R = 30% 
- If C > 30000 and <= 40000 then R = 40% 
+ If C > 5000 and <= 20000 then R = 20%
+ If C > 20000 and <= 30000 then R = 30%
+ If C > 30000 and <= 40000 then R = 40%
  If C > 40000 then R = 50% */
 
  #include<stdio.h>
@@ -23,7 +22,7 @@ Input cost of item. Rate of discount depends on cost.
             else
                 if(cost > 30000 && cost <= 40000)
                     rate = 40;
-                else 
+                else
                     rate = 50;
     discount = cost * rate / 100;
     final_cost = cost - discount;
