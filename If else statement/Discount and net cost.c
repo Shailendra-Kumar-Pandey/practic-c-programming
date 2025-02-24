f4ba77044033658cebee8f7e674142a9eb07d7d9
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-02-24 13:41:11 +0530

    Practic C

---
+++ If else statement/Discount and net cost.c
@@ -0,0 +1,7 @@
+/* Write to the Program calculate and print discount and net price.
+Input cost of item. Rate of discount depends on cost.
+ If C <= 5000 then R = 10%
+ If C > 5000 and <= 20000 then R = 20% 
+ If C > 20000 and <= 30000 then R = 30% 
+ If C > 30000 and <= 40000 then R = 40% 
+ If C > 40000 then R = 50% */
