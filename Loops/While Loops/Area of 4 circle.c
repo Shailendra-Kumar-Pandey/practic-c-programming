67845df1fc990af9e6027d8ea5342dc82ed42d6f
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-02-25 21:49:42 +0530

    Practic C

--- Loops/While Loops/Area of 4 circle.c
+++ Loops/While Loops/Area of 4 circle.c
@@ -3,5 +3,15 @@
 #include<stdio.h>
 int main ()
 {
-    
+    int i = 0;
+    float radius, area;
+    while (i<4)
+    {
+        printf("\n Enter the Value of Radius : ");
+        scanf("%f", &radius);
+        area = 3.14 * radius * radius;
+        printf("\n Circle of Area = %f", area);
+        i = i + 1;
+    }
+    return 0;
 }
