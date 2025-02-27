149e0309fdddfafbf8664995b366a402c427de3c
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-02-27 23:06:34 +0530

    Practic c

---
+++ Loops/While Loops/odd number 15.c
@@ -0,0 +1,13 @@
+/* To Generate and Print odd number 1st to 15 number */
+
+#include<stdio.h>
+int main()
+{
+    int i = -1;
+    while (i<15)
+    {
+        i = i +2;
+        printf("\n Odd Number is = %d",i);
+    }
+    return 0;    
+}
