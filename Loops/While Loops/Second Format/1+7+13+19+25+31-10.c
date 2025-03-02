8454379a093a165a80bc606b15f6951d386984e1
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-03-02 22:45:41 +0530

    Practic C

---
+++ Loops/While Loops/Second Format/1+7+13+19+25+31-10.c
@@ -0,0 +1,14 @@
+/* 1+7+13+19+25+31-10 calculate */
+
+#include<stdio.h>
+int main()
+{       /* CD= 6, i= i+6; i+6=1,i= 1-6, i=-5; s=-10, s= s+i */
+    int i= -5, s= -10;
+    while(i<35)
+        {
+            i= i+6;
+            s= s+i;
+        }
+    printf("\n Calculate is = %d",s);
+    return 0;
+}
