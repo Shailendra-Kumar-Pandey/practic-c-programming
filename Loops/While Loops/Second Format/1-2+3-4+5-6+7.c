e47a63a7ea3b396dbf66258edeb8d2423b014ff5
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-03-04 00:10:47 +0530

    Practic C

--- Loops/While Loops/Second Format/1-2+3-4+5-6+7.c
+++ Loops/While Loops/Second Format/1-2+3-4+5-6+7.c
@@ -1,7 +1,7 @@
 /* Calculate  1-2+3-4+5-6+7 */
 #include<stdio.h>
 int main()
-{           /* CD = 1, i= i+1; i+1=1, i=1-1, i=0; s= 0, s= s +t; t=0, t= i*sn; sn=1 */
+{           /* CD = 1, i= i+1; i+1=1, i=1-1, i=0; s= 0, s= s +t; t=0, t= i*sign; sign=1 */
     int i= 0, s= 0, t= 0, sign= 1;
     while (i<7)
     {
--- Loops/While Loops/Second Format/1-2-1-4+1-6-1-8+1-10-1-12+1-14.c
+++ Loops/While Loops/Second Format/1-2-1-4+1-6-1-8+1-10-1-12+1-14.c
@@ -3,14 +3,16 @@
 #include<stdio.h>
 int main()
 {
-    float i= 0, s= 0, t= 0, sign= 1, count= 0;
+    float i= 0, sum= 0, t= 0, sign= 1, count= 0;
     while (count<7)
-    {
+    {       /* CD= 2, i=i+2; i+2=2, i=2-2, i=0; t= (1/i)*sign, s=0, s=s+t; count=0, count= count+1; */
         i= i+2;
-
-        s= s + t;
+        t= (1/i)*sign;
+        sum= sum + t;
+        sign = sign * (-1);
+        count = count + 1;
         printf("\n number is = %f",t);
     }
-    printf("\n Calculate is = %f",s);
+    printf("\n Calculate is = %f",sum);
     return 0;
 }
