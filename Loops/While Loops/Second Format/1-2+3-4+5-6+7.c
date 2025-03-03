a7eaef811e179eea573b26a9032d602a6fdf7444
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-03-03 23:52:26 +0530

    Practic C

--- Loops/While Loops/Second Format/1-2+3-4+5-6+7.c
+++ Loops/While Loops/Second Format/1-2+3-4+5-6+7.c
@@ -1,16 +1,14 @@
 /* Calculate  1-2+3-4+5-6+7 */
 #include<stdio.h>
 int main()
-{           /* CD = 1, i= i+1; i+1=1, i=1-1, i=0; s= 0, s= s +t; t=0, t= i*-1; */
-    int i= 0, s= 0,t= 0;
+{           /* CD = 1, i= i+1; i+1=1, i=1-1, i=0; s= 0, s= s +t; t=0, t= i*sn; sn=1 */
+    int i= 0, s= 0, t= 0, sign= 1;
     while (i<7)
     {
         i= i+1;
-        if (i%2==0)
-            t = i*-1;
-        else
-            t = i;
+        t= i*sign;
         s = s + t;
+        sign= sign*(-1);
         printf("\n Show Number = %d",t);
     }
     printf("\n Calculate is =%d",s);
