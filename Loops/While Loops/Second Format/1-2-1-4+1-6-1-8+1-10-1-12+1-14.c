f57f9736f6cd8481dc850fd480b7c31502c2f10d
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-03-03 23:59:33 +0530

    Practic c

--- Loops/While Loops/Second Format/1-2-1-4+1-6-1-8+1-10-1-12+1-14.c
+++ Loops/While Loops/Second Format/1-2-1-4+1-6-1-8+1-10-1-12+1-14.c
@@ -1,17 +1,13 @@
-/* 1/2-1/4+1/6-1/8+1/10-1/12+1/14 Calculate sum */
+/* 1/2-1/4+1/6.... 7 terms Calculate sum */
 
 #include<stdio.h>
 int main()
 {
-    int i=0;
-    float s= 0,t= 0;
-    while (i<14)
+    float i= 0, s= 0, t= 0, sign= 1, count= 0;
+    while (count<7)
     {
         i= i+2;
-        if(i%4==0)
-            t= (1.0/i)*(-1);
-        else
-            t = (1.0/i)*(1);
+
         s= s + t;
         printf("\n number is = %f",t);
     }
