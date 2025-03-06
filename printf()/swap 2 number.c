aaf909d48006f6442bbe07fb3f177fdf7da43509
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-03-06 23:44:21 +0530

    Practic c

---
+++ printf()/swap 2 number.c
@@ -0,0 +1,14 @@
+/* Write a program given number in 2 Number Swap */
+
+#include<stdio.h>
+int main()
+{
+    int a, b;
+    printf("\n Enter the two number :");
+    scanf("%d,%d",&a,&b);
+    if(a<b)
+        printf("\n this is a assending order = %d, %d",a,b);
+    else
+        printf("\n this is a assending order = %d, %d",b,a);
+    return 0;
+}
