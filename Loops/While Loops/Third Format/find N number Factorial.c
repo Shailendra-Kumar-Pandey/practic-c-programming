f3be8543b2c719c6266bd4fb75e36de60a4f37de
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-03-07 23:53:37 +0530

    Practic C

---
+++ Loops/While Loops/Third Format/find N number Factorial.c
@@ -0,0 +1,24 @@
+/* Write a Program to Calculate and print Factorial N numbers */
+
+#include<stdio.h>
+int main()
+{
+    float i=0, j=0, t=1, n, m;
+    printf("\n How many Numbers of print Factorial : ");
+    scanf("%f",&m);
+    while (j<m)
+    {
+        j = j +1;
+        printf("\n Enter the Number is Factor :");
+        scanf("%f",&n);
+        while (i<n)
+        {
+            i = i+1;
+            t = t*i;
+        }
+        printf("\n Factorial is = %f",t);
+        i = 0;
+        t = 1;
+    }
+    return 0;
+}
