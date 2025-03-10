50392111166f63cbbd7bf559d1f22ca2525403d2
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-03-11 02:13:22 +0530

    Practic C

--- Loops/While Loops/Third Format/1!+3!+5!+7!+9!.c
+++ Loops/While Loops/Third Format/1!+3!+5!+7!+9!.c
@@ -2,14 +2,14 @@
 
 #include<stdio.h>
 int main()
-{       /* CD=2; i=i+2, i+2=1, i=1-2, i=-1; t=1, t=t*i; */
+{       /* CD=2; i=i+2, i+2=1, i=1-2, i=-1; t=1, t=t*i*(i-1); */
     int i= -1, t=1,s=0;
     while (i<9)
     {
         i = i+2;
-        t = t*i;
+        t = t*i*(i+1);
         s = s+t;
-        printf("\n %d ",i);
+        printf("\n %d %d %d ",i,t,s);
     }
     printf("\n\n Factorial is = %d",s);
     return 0;
--- Loops/While Loops/Third Format/2!+4!+6!+8!.c
+++ Loops/While Loops/Third Format/2!+4!+6!+8!.c
@@ -2,12 +2,12 @@
 
 #include<stdio.h>
 int main()
-{       /* CD=2; i=i+2, i+2=2, i=2-2, i=0; t=1, t=t*i; */
+{       /* CD=2; i=i+2, i+2=2, i=2-2, i=0; t=1, t=t*i*(i-1); */
     int i= 0, t=1,s=0;
     while (i<8)
     {
         i = i+2;
-        t = t*i;
+        t = t*i*(i-1);
         s = s+t;
         printf("\n %d ",i);
     }
