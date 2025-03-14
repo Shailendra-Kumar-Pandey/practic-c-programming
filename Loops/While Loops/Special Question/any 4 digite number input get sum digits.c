5905d6131be0a9756939e279a1abbafab9927467
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-03-14 19:12:49 +0530

    Practic C

--- Loops/While Loops/Special Question/any 4 digite number input get sum digits.c
+++ Loops/While Loops/Special Question/any 4 digite number input get sum digits.c
@@ -1,12 +1,55 @@
-/* Write a Program to input 4 Digit Number and print sum of its digits. 2671 = 16 */
+/* 1st Method:- Write a Program to input 4 Digit Number and print sum of its digits.
+ 2671 = 16
 
 #include<stdio.h>
 int main()
 {
-    int a,b,c,d,sum;
+    int a , d1, d2, d3, d4, sum;
     printf("\n Enter only Four Digit Number : ");
-    scanf("%d%d%d%d",&a,&b,&c,&d);
-    sum = a+b+c+d;
+    scanf("%d",&a);
+    d1 = a/1000;
+    d2 = (a-(d1*1000))/100;
+    d3 = (a-(d1*1000+d2*100))/10;
+    d4 = (a-(d1*1000+d2*100+d3*10));
+    sum = d1+ d2+ d3+ d4;
     printf("\n %d",sum);
+    return 0;
+}
+
+ 2nd Method:-
+#include<stdio.h>
+int main()
+{
+
+    int i=0, x , d, sum1=0;
+    printf("\n Enter only Four Digit Number : ");
+    scanf("%d",&x);
+    while(i<4)
+    {
+        d = x%10;
+        sum1 = sum1 + d;
+        x = x/10;
+        i = i +1;
+    }
+    printf("\n %d",sum1);
+    return 0;
+}
+
+3rd Method
+*/
+
+#include<stdio.h>
+int main()
+{
+    int x , d, sum1=0;
+    printf("\n Enter any Digit Number : ");
+    scanf("%d",&x);
+    while(x>0)
+    {
+        d = x%10;
+        sum1 = sum1 + d;
+        x = x/10;
+    }
+    printf("\n %d",sum1);
     return 0;
 }
