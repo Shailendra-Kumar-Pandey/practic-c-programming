2f4e768e32862f561ac8189b90a5a8e1795d18a7
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-03-18 20:35:07 +0530

    Practic C

--- Loops/Do...while Loops/company employee count.c
+++ Loops/Do...while Loops/company employee count.c
@@ -4,29 +4,21 @@ Consider codes as for male employee = 1, for female employee = 2, To stop =3. */
 #include<stdio.h>
 int main()
 {
-    int  coman, male=0, female=0, stop=0, total=0;
+    int  entry=0, male=0, female=0, total=0;
     do
     {
         printf("\n Please Enter the Employee code Male-1 and Female-2 and Entry Complet Enter 3 : ");
-        scanf("%d",&coman);
-        if (coman == 1)
-        {
+        scanf("%d",&entry);
+        if (entry == 1)
              male = male + 1;
-        }else{
-            if(coman == 2)
-            {
+        else
+            if(entry == 2)
                 female = female + 1;
-            }else if(coman == 3)
-                {
-                    stop = 3;
-                }else{
+            else
+                if(entry<1 || entry>3)
                     printf("\n this Employee code is Not Valid, Please Valid Number ");
-                    }
-        }
-    }while (stop != 3);
+    }while(entry != 3);
             total = male + female;
-    printf("\n Company are male Employees Entry = %d",male);
-    printf("\n Company are female Employees Entry = %d",female);
-    printf("\n Company are Total Employees Entry = %d",total);
+    printf("\n males = %d  females = %d \n Total Employees = %d ", male, female, total);
     return 0;
 }
