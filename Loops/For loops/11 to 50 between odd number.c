554186a6a8fe6e64c24b39552466b149c6425367
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-03-19 22:46:49 +0530

    Practic c

---
+++ Loops/For loops/11 to 50 between odd number.c
@@ -0,0 +1,11 @@
+/* Write a Program to generate and print between 11 to 50 odd Number */
+#include<stdio.h>
+int main()
+{
+    int i;
+    for ( i = 13; i <50; i=i+2)
+    {
+        printf(" %d  ",i);
+    }
+    return 0;
+}
