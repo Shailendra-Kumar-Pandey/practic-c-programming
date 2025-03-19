9ce6f87ef95ce9dbbcc664751cfeb62188588df1
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-03-19 22:31:56 +0530

    Practic C

---
+++ Loops/For loops/first natural Number.c
@@ -0,0 +1,11 @@
+/* Write a Program to generate and print first 10 Natural Number */
+#include<stdio.h>
+int main()
+{
+    int i;
+    for ( i = 1; i <=10; i=i+1)
+    {
+        printf(" %d  ",i);
+    }
+    return 0;
+}
