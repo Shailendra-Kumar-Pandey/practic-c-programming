0392d818022d30aace7b28e8eb606d2b463d7b69
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-03-20 20:28:12 +0530

    Practic C

--- Loops/Switch Statement/Ip Num print month name.c
+++ Loops/Switch Statement/Ip Num print month name.c
@@ -11,13 +11,13 @@ int main()
         printf("\n Month is January...");
         break;
     case 2:
-        printf("\n Month is Fabuary...");
+        printf("\n Month is February...");
         break;
     case 3:
         printf("\n Month is March...");
         break;
     case 4:
-        printf("\n Month is Apral...");
+        printf("\n Month is April...");
         break;
     case 5:
         printf("\n Month is May...");
@@ -29,16 +29,16 @@ int main()
         printf("\n Month is July...");
         break;
     case 8:
-        printf("\n Month is Augest...");
+        printf("\n Month is August...");
         break;
     case 9:
         printf("\n Month is September...");
         break;
     case 10:
-        printf("\n Month is Octuber...");
+        printf("\n Month is October...");
         break;
     case 11:
-        printf("\n Month is Nebember...");
+        printf("\n Month is November...");
         break;
     case 12:
         printf("\n Month is December...");
