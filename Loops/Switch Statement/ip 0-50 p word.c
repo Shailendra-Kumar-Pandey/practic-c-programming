28eba37186544c2ec42a01a760a2a620146273e9
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-03-20 22:12:44 +0530

    Practic c

---
+++ Loops/Switch Statement/ip 0-50 p word.c
@@ -0,0 +1,167 @@
+/* Write a program to input number 0 to 50 then Print Words  */
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("\n Enter the Number (0-50) :");
+    scanf("%d",&num);
+    switch (num)
+    {
+    case 0:
+        printf("\n Number is Zero...");
+        break;
+    case 1:
+        printf("\n Number is One...");
+        break;
+    case 2:
+        printf("\n Number is Two...");
+        break;
+    case 3:
+        printf("\n Number is Three...");
+        break;
+    case 4:
+        printf("\n Number is Four...");
+        break;
+    case 5:
+        printf("\n Number is Five...");
+        break;
+    case 6:
+        printf("\n Number is Six...");
+        break;
+    case 7:
+        printf("\n Number is Seven...");
+        break;
+    case 8:
+        printf("\n Number is Eight...");
+        break;
+    case 9:
+        printf("\n Number is Nine...");
+        break;
+    case 10:
+        printf("\n Number is Ten...");
+        break;
+    case 11:
+        printf("\n Number is Eleven...");
+        break;
+    case 12:
+        printf("\n Number is Twelve...");
+        break;
+    case 13:
+        printf("\n Number is Thirteen...");
+        break;
+    case 14:
+        printf("\n Number is Fourteen...");
+        break;
+    case 15:
+        printf("\n Number is Fifteen...");
+        break;
+    case 16:
+        printf("\n Number is Sixteen...");
+        break;
+    case 17:
+        printf("\n Number is Seventeen...");
+        break;
+    case 18:
+        printf("\n Number is Eighteen...");
+        break;
+    case 19:
+        printf("\n Number is Nineteen...");
+        break;
+    case 20:
+        printf("\n Number is Twenty...");
+        break;
+    case 21:
+        printf("\n Number is Twenty-One...");
+        break;
+    case 22:
+        printf("\n Number is Twenty-Two...");
+        break;
+    case 23:
+        printf("\n Number is Twenty-Three...");
+        break;
+    case 24:
+        printf("\n Number is Twenty-Four...");
+        break;
+    case 25:
+        printf("\n Number is Twenty-Five...");
+        break;
+    case 26:
+        printf("\n Number is Twenty-Six...");
+        break;
+    case 27:
+        printf("\n Number is Twenty-Seven...");
+        break;
+    case 28:
+        printf("\n Number is Twenty-Eight...");
+        break;
+    case 29:
+        printf("\n Number is Twenty-Nine...");
+        break;
+    case 30:
+        printf("\n Number is Thirty...");
+        break;
+    case 31:
+        printf("\n Number is Thirty-One...");
+        break;
+    case 32:
+        printf("\n Number is Thirty-Two...");
+        break;
+    case 33:
+        printf("\n Number is Thirty-Three...");
+        break;
+    case 34:
+        printf("\n Number is Thirty-Four...");
+        break;
+    case 35:
+        printf("\n Number is Thirty-Five...");
+        break;
+    case 36:
+        printf("\n Number is Thirty-Six...");
+        break;
+    case 37:
+        printf("\n Number is Thirty-Seven...");
+        break;
+    case 38:
+        printf("\n Number is Thirty-Eight...");
+        break;
+    case 39:
+        printf("\n Number is Thirty-Nine...");
+        break;
+    case 40:
+        printf("\n Number is Forty...");
+        break;
+    case 41:
+        printf("\n Number is Forty-One...");
+        break;
+    case 42:
+        printf("\n Number is Forty-Two...");
+        break;
+    case 43:
+        printf("\n Number is Forty-Three...");
+        break;
+    case 44:
+        printf("\n Number is Forty-Four...");
+        break;
+    case 45:
+        printf("\n Number is Forty-Five...");
+        break;
+    case 46:
+        printf("\n Number is Forty-Six...");
+        break;
+    case 47:
+        printf("\n Number is Forty-Seven...");
+        break;
+    case 48:
+        printf("\n Number is Forty-Eight...");
+        break;
+    case 49:
+        printf("\n Number is Forty-Nine...");
+        break;
+    case 50:
+        printf("\n Number is Fifty...");
+        break;
+    default:
+        printf("\n Please Enter Valid Number...");
+    }
+    return 0;
+}
