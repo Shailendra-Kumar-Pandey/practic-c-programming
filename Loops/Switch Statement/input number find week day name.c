be7cd0fa69af8328c9818ed228424dc279a3080b
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-03-20 20:01:56 +0530

    Practic C

---
+++ Loops/Switch Statement/input number find week day name.c
@@ -0,0 +1,35 @@
+/* Write a Program to input day number print its equ day name */
+#include<stdio.h>
+int main()
+{
+    int dn;
+    printf("\n Enter the Day Number (1-7) :");
+    scanf("%d",&dn);
+    switch (dn)
+    {
+    case 1:
+        printf("\n Day is Sunday...");
+        break;
+    case 2:
+        printf("\n Day is Monday...");
+        break;
+    case 3:
+        printf("\n Day is Tuesday...");
+        break;
+    case 4:
+        printf("\n Day is Wednesday...");
+        break;
+    case 5:
+        printf("\n Day is Thursday...");
+        break;
+    case 6:
+        printf("\n Day is Friday...");
+        break;
+    case 7:
+        printf("\n Day is Saturday...");
+        break;
+    default:
+        printf("\n Invailid Day Number...");
+    }
+    return 0;
+}
