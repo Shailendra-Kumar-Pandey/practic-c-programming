c68858e6411fb1f984ede48537e782c15113dd22
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-03-21 23:38:36 +0530

    Practic C

--- Loops/Switch Statement/discount and net price.c
+++ Loops/Switch Statement/discount and net price.c
@@ -60,3 +60,55 @@ int main()
     } while (gc!=7);
     return 0;
 }
+
+// #include<stdio.h>
+// int main()
+// {
+//     int gc;
+//     float r, d, c, nc;
+//         d = 0, nc = 0;
+//         printf("\n Enter Group Code (1-7) :");
+//         scanf("%d",&gc);
+//         switch (gc)
+//             {
+//                 case 1:
+//                         printf("\n Enter Cost Value :");
+//                         scanf("%f",&c);
+//                         r =10;
+//                         d = c * r /100;
+//                         nc = c - d;
+//                         printf("\n Discount = %f  Net-Cost = %f ",d,nc);
+//                         break;
+//                 case 2:
+//                 case 3:
+//                         printf("\n Enter Cost Value :");
+//                         scanf("%f",&c);
+//                         r = 20;
+//                         d = c * r /100;
+//                         nc = c - d;
+//                         printf("\n Discount = %f  Net-Cost = %f ",d,nc);
+//                         break;
+//                 case 4:
+//                 case 5:
+//                         printf("\n Enter Cost Value :");
+//                         scanf("%f",&c);
+//                         r = 30;
+//                         d = c * r /100;
+//                         nc = c - d;
+//                         printf("\n Discount = %f  Net-Cost = %f ",d,nc);
+//                         break;
+//                 case 6:
+//                         printf("\n Enter Cost Value :");
+//                         scanf("%f",&c);
+//                         r = 40;
+//                         d = c * r /100;
+//                         nc = c - d;
+//                         printf("\n Discount = %f  Net-Cost = %f ",d,nc);
+//                         break;
+//                 case 7:
+//                         break;
+//                 default:
+//                         printf("\n Invalid Group Code...");
+//             }
+//     return 0;
+// }
