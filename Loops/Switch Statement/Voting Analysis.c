f161b30f5fcd75ab6f7b07ed3b69640044ebd757
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-03-24 16:33:49 +0530

    Practic C

--- Loops/Switch Statement/Voting Analysis.c
+++ Loops/Switch Statement/Voting Analysis.c
@@ -4,23 +4,32 @@
  #include<stdio.h>
  int main()
  {
-    int e, v, w;
+    int bjp=0, cog=0, aap=0, sp=0, bsp=0, v;
     do
     {
-        printf("\n Enter your Vote of Interest Condidate (1-5) : ");
+        printf("\n Enter your Vote of Interest Condidat (1-5) : ");
         scanf("%d",&v);
         switch (v)
         {
         case 1:
-            
+            bjp = bjp + 1;
+            printf("\n Your Vote in BJP...");
             break;
         case 2:
+            cog = cog + 1;
+            printf("\n Your Vote in COG...");
             break;
         case 3:
+            aap = aap + 1;
+            printf("\n Your Vote in AAP...");
             break;
         case 4:
+            sp = sp + 1;
+            printf("\n Your Vote in SP...");
             break;
         case 5:
+            bsp = bsp + 1;
+            printf("\n Your Vote in BSP...");
             break;
         case 6:
             break;
@@ -29,9 +38,20 @@
         }
         if (v == 6)
         {
-            w = 
+             if(bjp>cog && bjp>aap && bjp>sp && bjp>bsp)
+                printf("\n The Winner is BJP with %d Votes",bjp);
+            else
+                if (cog>aap && cog>sp && cog>bsp)
+                    printf("\n The Winner is COG with %d Votes",cog);
+                else
+                    if(aap>sp && aap>bsp)
+                        printf("\n The Winner is AAP with %d Votes",aap);
+                    else
+                        if(sp>bsp)
+                            printf("\n The Winner is SP with %d Votes",sp);
+                        else
+                            printf("\nThe Winner is BSP with %d Votes",bsp);
         }
     } while (v!=6);
-    
-
+    return 0;
  }
