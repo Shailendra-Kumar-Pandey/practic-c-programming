2e8848fedbe33a51f6ff534e1957a9f5ed3d2a40
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-03-24 20:51:57 +0530

    Practic C

--- Loops/Switch Statement/Voting Analysis.c
+++ Loops/Switch Statement/Voting Analysis.c
@@ -7,48 +7,50 @@
     int bjp=0, cog=0, aap=0, sp=0, bsp=0, v;
     do
     {
-        printf("\n Enter your Vote of Interest Condidat (1-5) : ");
+        printf("\nEnter your Vote of Interest Condidat (1-5) : ");
         scanf("%d",&v);
         switch (v)
         {
         case 1:
             bjp = bjp + 1;
-            printf("\n Your Vote in BJP...");
+            printf("\nYour Vote in BJP...");
             break;
         case 2:
             cog = cog + 1;
-            printf("\n Your Vote in COG...");
+            printf("\nYour Vote in COG...");
             break;
         case 3:
             aap = aap + 1;
-            printf("\n Your Vote in AAP...");
+            printf("\nYour Vote in AAP...");
             break;
         case 4:
             sp = sp + 1;
-            printf("\n Your Vote in SP...");
+            printf("\nYour Vote in SP...");
             break;
         case 5:
             bsp = bsp + 1;
-            printf("\n Your Vote in BSP...");
+            printf("\nYour Vote in BSP...");
             break;
         case 6:
+            printf("\nBJP Votes = %d \nCOG Votes = %d \nAAP Votes = %d \nSP Votes = %d \nBSP Votes = %d", bjp, cog, aap, sp, bsp);
             break;
         default:
-            printf("\n Invalid Vote Please Enter Valid Vote...");
+            printf("\nInvalid Vote Please Enter Valid Vote...");
         }
         if (v == 6)
         {
+            printf("\nFinal Result...");
              if(bjp>cog && bjp>aap && bjp>sp && bjp>bsp)
-                printf("\n The Winner is BJP with %d Votes",bjp);
+                printf("\nThe Winner is BJP with %d Votes",bjp);
             else
                 if (cog>aap && cog>sp && cog>bsp)
-                    printf("\n The Winner is COG with %d Votes",cog);
+                    printf("\nThe Winner is COG with %d Votes",cog);
                 else
                     if(aap>sp && aap>bsp)
-                        printf("\n The Winner is AAP with %d Votes",aap);
+                        printf("\nThe Winner is AAP with %d Votes",aap);
                     else
                         if(sp>bsp)
-                            printf("\n The Winner is SP with %d Votes",sp);
+                            printf("\nThe Winner is SP with %d Votes",sp);
                         else
                             printf("\nThe Winner is BSP with %d Votes",bsp);
         }
