936e7d4bf201288a065c5ceb04990c511865afef
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-03-28 23:53:41 +0530

    Practic C

--- Loops/For loops/1st prime Number.c
+++ Loops/For loops/1st prime Number.c
@@ -1,15 +1,17 @@
 /* Write a Program to generate and print Prime Number 1st up to 100 */
 #include<stdio.h>
 int main()
-{   /* 2, 3, 5, 7, 11, 13, 17, 19, 23, 27,  */
-    int i, n, a=2, b=3, c=5, d=7;
-    printf("\n How many number prime Number find :");
-    scanf("%d",&n);
-    printf(" %d, %d, %d, %d",a,b,c,d);
-    for ( i = 2; i < n; i=i+1)
+{   /* 2, 3, 5, 7, 11, 13, 17, 19, 23, 29,  */
+    int j=2,i=2;
+    for(i=2;i<100;i++)
     {
-        if (i%2!=0 && i%3!=0 && i%5!=0 && i%7!=0)
-            printf(", %d",i);
+        for(j=2;j<i;j++)
+        {
+            if(i%j==0)
+                break;
+        }
+        if(i==j)
+            printf(" %d ",i);
     }
     return 0;
 }
