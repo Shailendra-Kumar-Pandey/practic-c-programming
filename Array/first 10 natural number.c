700c24579df1a73a6dd498dcaf81e73bda43abe8
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-04-11 22:45:47 +0530

    Pracrtic c

--- Array/first 10 natural number.c
+++ Array/first 10 natural number.c
@@ -1,1 +1,11 @@
 /* create an array of size 10 load it with first 10 natural number  */
+#include<stdio.h>
+int main()
+{
+    int a[10],i;
+    for ( i = 0; i <= 9; i++)
+        a[i]=i+1;
+    for ( i = 0; i <= 9; i++)
+        printf(" %d ",a[i]);
+    return 0;   
+}
