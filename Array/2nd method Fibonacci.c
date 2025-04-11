79a5d5b453c6785e9994cf499738a81866e100a6
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-04-12 00:15:28 +0530

    practic c

--- Array/2nd method Fibonacci.c
+++ Array/2nd method Fibonacci.c
@@ -5,17 +5,19 @@ int main()
     int a[10],i,f=0,s=1,t=0;
     for ( i = 0; i <= 9; i++)
     {
+         t = f + s;
         if(i==0)
             a[i]= f;
-        if(i==1)
-            a[i]= s;
-         t = f + s;
-        if(i>1 && i<=9)
-        {
-            a[i] = t;
-            f = s;
-            s = t;
-        }
+        else
+            if(i==1)
+                a[i]= s;
+            else
+                if(i>1 && i<=9)
+                 {
+                     a[i] = t;
+                     f = s;
+                     s = t;
+                 }
     }
     for ( i = 0; i <= 9; i++)
         printf(" %d ",a[i]);
