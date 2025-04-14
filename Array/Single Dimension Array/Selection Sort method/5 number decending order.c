9a8b767b6407fadf91b9f17c783f1b0d952875b0
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-04-14 20:48:37 +0530

    practic C

--- Array/Single Dimension Array/Selection Sort method/5 number decending order.c
+++ Array/Single Dimension Array/Selection Sort method/5 number decending order.c
@@ -1,28 +1,27 @@
-/* Create an array of size 05 inputting number and show decending order print array */
+/* Create an array of size 05 inputting number and show descending order print array */
 #include<stdio.h>
 int swap(int,int);
 int main()
 {
-    int a[5],i,n,j;
-    for ( i = 0; i < 5; i++)
+    int a[5],i,j,big;
+    for ( i = 0; i <= 4; i++)
     {
-        printf("\n Enter the NUmber :");
-        scanf("%d",&n);
-        a[i] = n;
-        for ( j = 0; j > i; j++)
+        printf("\n Enter the Number :");
+        scanf("%d",&a[i]);
+    }
+    for ( i = 0; i <=3; i++)
+    {
+        for ( j = i+1; j <=4 ; j++)
         {
-            int big;
-            if (a[i]>a[i+1])
+            if (a[j]>a[i])
             {
-                a[i] = a[i];
-            }else{
-                big = a[i+1];
-                a[i+1]=a[i];
-                a[i]=big;
+                big = a[i];
+                a[i]=a[j];
+                a[j]=big;
             }
         }
     }
-    for ( i = 0; i < 5; i++)
+    for ( i = 0; i <= 4; i++)
         printf(" %d ",a[i]);
     return 0;
 }
