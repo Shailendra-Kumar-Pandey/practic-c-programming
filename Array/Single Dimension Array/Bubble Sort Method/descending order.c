abc72dffb540d7293a031b7d8b384b11a6ecbdf5
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-04-15 21:14:35 +0530

    Practic C

---
+++ Array/Single Dimension Array/Bubble Sort Method/descending order.c
@@ -0,0 +1,28 @@
+/* Write a program to create an array of size 5 inputting user number and print to array sort
+ descending order (Using Bubble Sort Method) */
+ #include<stdio.h>
+ int main()
+ {
+    int a[5],i,j,b;
+    for ( i = 0; i <= 4; i++)
+      {
+        printf("\nEnter the Number : ");
+        scanf("%d",&a[i]);
+      }
+    for ( i = 0; i <= 3; i++)
+    {
+        for ( j = 0; j <= 3-i; j++)
+        {
+            if (a[j+1]>a[j])
+            {
+                b = a[j];
+                a[j] = a[j+1];
+                a[j+1] = b;
+            }
+        }
+    }
+    printf("\n Bubble Sort array element \n");
+    for ( i = 0; i <= 4; i++)
+        printf(" %d ",a[i]);
+    return 0;
+ }
