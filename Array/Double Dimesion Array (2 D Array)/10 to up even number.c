ce7a5e84079eb50d2ae83fc4ee16f3136a7c8b4f
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-04-15 21:48:44 +0530

    Practic C

---
+++ Array/Double Dimesion Array (2 D Array)/10 to up even number.c
@@ -0,0 +1,18 @@
+/* Create an array 4 by 4 at load at with even Number 10 to up finally print array */
+#include<stdio.h>
+int main()
+{
+    int a[4][4],i,j,e=10;
+    for ( i = 0; i <= 3 ; i++)
+    {
+        for ( j = 0; j <= 3; j++)
+        {
+            a[i][j] = e;
+            e = e + 2;
+        }
+    }
+    printf("\n Array are Ever Number element:- \n");
+    for ( i = 0; i <= 3; i++)
+        printf("\n %d \t%d \t%d \t%d",a[i][0],a[i][1],a[i][2],a[i][2]);
+    return 0;
+}
