fcedfba01d15328e24ddde4908c3a1d5975f74a5
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-04-17 00:06:22 +0530

    Practic C

---
+++ Array/Specal Question/Sum 1st diagonal elements(trace of matrix).c
@@ -0,0 +1,22 @@
+/* Write a program to create an array of size 4 by 4 load array with any numbers.
+ Print array as will as sum of 1st diagonal elements (trace of matrix) */
+ #include<stdio.h>
+int main()
+{
+    int a[4][4],i,j,sum=0;
+    for ( i = 0; i <= 3; i++)
+    {
+        for ( j = 0; j <= 3; j++)
+        {
+            printf("\n Enter the Number :");
+            scanf("%d",&a[i][j]);
+            if ((i==0 && j==0) || (i==1 && j==1) || (i==2 && j==2) || (i==3 && j==3))
+                sum = sum + a[i][j];
+        }
+    }
+    printf("\n Array Element are \n");
+    for ( i = 0; i <= 3; i++)
+        printf("\n%d \t%d \t%d \t%d",a[i][0],a[i][1],a[i][2],a[i][3]);
+    printf("\n\n 1st diagonal element Sum = %d",sum);
+    return 0;
+}
--- Array/Specal Question/roll no and total marks sort merits.c
+++ Array/Specal Question/roll no and total marks sort merits.c
@@ -1,5 +1,5 @@
-/* Create array storage the roll no and Total marks 15 student finally print marite list in a
-formate show on bwlow */
+/* Create array storage the roll no and Total marks 15 student finally print merit list in a
+formate show on below */
 
 #include<stdio.h>
 int main()
@@ -7,7 +7,6 @@ int main()
     int i, j, swap,big;
     int roll_no[15] = {111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125};
     int total_marks[15] = {511, 248, 560, 520, 480, 380, 360, 590, 565, 320, 311, 440, 375, 580, 250};
-    int merit_list[15] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
     for ( i = 0; i <= 13; i++)
         {
             for ( j = i+1; j <= 14; j++)
@@ -25,6 +24,6 @@ int main()
         }
         printf("\n Merit List   Roll No. \tTotal Marks\n");
         for ( i = 0; i <= 14; i++)
-            printf("\n\t%d  \t%d \t  %d",merit_list[i],roll_no[i],total_marks[i]);
+            printf("\n\t%d  \t%d \t  %d",i+1,roll_no[i],total_marks[i]);
     return 0;
 }
