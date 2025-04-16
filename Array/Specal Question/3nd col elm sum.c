cf582ec63be1d3cbe3733702afd6887e7eb6d8ca
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-04-16 23:32:42 +0530

    Practic C

---
+++ Array/Specal Question/3nd col elm sum.c
@@ -0,0 +1,22 @@
+/* Write a program to create an array of size 4 by 4 load array with any numbers.
+ Print array as will as sum of 3rd column Elements */
+ #include<stdio.h>
+int main()
+{
+    int a[4][4],i,j,sum=0;
+    for ( i = 0; i <= 3; i++)
+    {
+        for ( j = 0; j <= 3; j++)
+        {
+            printf("\n Enter the Number :");
+            scanf("%d",&a[i][j]);
+            if (j==2)
+                sum = sum + a[i][j];
+        }
+    }
+    printf("\n Array Element are \n");
+    for ( i = 0; i <= 3; i++)
+        printf("\n%d \t%d \t%d \t%d",a[i][0],a[i][1],a[i][2],a[i][3]);
+    printf("\n\n 3st column element Sum = %d",sum);
+    return 0;
+}
