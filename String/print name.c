aab6aa7fdf3d918e504ed00f31e4ba6fb2c1acfa
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-04-18 22:28:01 +0530

    practic C

---
+++ String/print name.c
@@ -0,0 +1,14 @@
+/* write a program to create an array to finally print to name */
+#include<stdio.h>
+int main()
+{
+    char a[11] = {'S','H','A','I','L','E','N','D','R','A','\0'};
+    int i = 0;
+    printf("\nHello! ");
+    while (a[i] != '\0')
+    {
+        printf("%c",a[i]);
+        i = i+1;
+    }
+    return 0;
+}
