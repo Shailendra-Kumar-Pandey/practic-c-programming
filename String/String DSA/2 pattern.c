b6609f7fccef2813a3def116d544778e33639e46
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-04-22 00:44:04 +0530

    Practic C

--- String/String DSA/2 pattern.c
+++ String/String DSA/2 pattern.c
@@ -5,20 +5,12 @@ Eg:- JEETU
 #include<stdio.h>
 int main()
 {
-    char fname[30],b[30];
-    int i,j,l;
+    char fname[30];
+    int i,l;
     printf("\nEnter the First Name :-");
     scanf("%s",fname);
     l = strlen(fname);
-    for ( i = 0; i < l; i++)
-    {
-        for ( j = 0; j <=i ; j++)
-        {
-            b[j] = fname[l-1];
-        }
-        //printf("\n");
-         printf("\n %c",b[i]);
-    }
-
+    for ( i = l; i>=0; i=i-1)
+        printf("%c",fname[i]);
     return 0;
 }
