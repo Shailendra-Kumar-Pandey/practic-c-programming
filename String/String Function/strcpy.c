16d071ba511fceb7945324b7bd73759e4b38f882
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-04-21 23:13:55 +0530

    Practic C

---
+++ String/String Function/strcpy.c
@@ -0,0 +1,11 @@
+/* Write a program to using string function {strlen(<string name>)}  */
+#include<stdio.h>
+int main()
+{
+    char source[]= "Shailendra Kumar Pandey";
+    char target[30];
+    strcpy(target,source);
+    printf("\nSource String = %s",source);
+    printf("\nTarget String = %s",target);
+    return 0;
+}
