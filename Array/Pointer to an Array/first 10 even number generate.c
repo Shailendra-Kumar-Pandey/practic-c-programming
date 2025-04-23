12f1157630b1a15ad68d9d327ad4a168aeaccf79
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-04-23 23:02:24 +0530

    Practic C

---
+++ Array/Pointer to an Array/first 10 even number generate.c
@@ -0,0 +1,18 @@
+/* To generate and print first 10 even number {Using Pointer an Array} */
+#include<stdio.h>
+int main()
+{
+    int a[10],i,j=0,*p;
+    for ( i = 0; i <= 9; i++)
+    {
+        j = j+2;
+        a[i] = j;
+    }
+    p = &a[0];
+    printf("\nArray elenment are\n");
+    for ( i = 0; i <=9 ; i++)
+    {
+        printf("\n Value=%d Value=%d Address=%u",*(p+i),a[i],p+i);
+    }
+    return 0;    
+}
