26a8284ea855b646edb16f514f49469b640679f8
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-04-23 21:05:14 +0530

    Practic C

--- String/Special Question/pailidrom word or not find.c
+++ String/Special Question/pailidrom word or not find.c
@@ -6,21 +6,12 @@ int main()
     int l, n, i;
     printf("\n Enter the any word:- ");
     scanf("%s",word);
-    printf("\n Enter the any word:- ");
-    scanf("%s",b);
-    if(b==word)
-        printf("ok");
-     else
-        printf("not ok");
     l = strlen(word);
-   printf("\n %d",l);
-    for ( i = l-1; i>=0; i--)
+    for ( i = l; i>=0; i--)
      {
          b[i]=word[i];
          printf("%c",b[i]);
      }
-     n = strlen(b);
-     printf("%d",n);
      if(b==word)
         printf("ok");
      else
