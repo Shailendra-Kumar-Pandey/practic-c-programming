9fc1fe5196ae15ef91ffea37624367cbe8d8fad0
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-05-01 21:57:32 +0530

    Practic C

---
+++ Function/M-3 2 out of biggest.c
@@ -0,0 +1,18 @@
+/* Write a Program to Compare and Print of Biggest out of 2 Number */
+// Method No.03
+#include<stdio.h>
+void big();
+int main()
+{
+    big();
+    return 0;
+}
+void big()
+{    int x, y;
+    printf("\nEnter the Number :");
+    scanf("%d%d",&x,&y);
+    if(x>y)
+        printf("\nBiggest Number is = %d",x);
+    else
+        printf("\nBiggest Number is = %d",y);
+}
