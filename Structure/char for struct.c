87af1dab0bcebab945ccdf597156eddb034a0503
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-05-22 22:11:12 +0530

    Practic c

---
+++ Structure/char for struct.c
@@ -0,0 +1,21 @@
+/* Write a program usage of an array of structure  store 5 book data */
+#include<stdio.h>
+int main()
+{
+    struct book
+    {
+        char name;
+        float price;
+        int pages;
+    };
+    struct book b[5];
+    int i;
+    for(i=0;i<=4;i++)
+    {
+        printf("\nEnter the Book Name, Price and Pages : ");
+        scanf(" %c %f %d",&b[i].name,&b[i].price,&b[i].pages);
+    }
+    for(i=0;i<=4;i++)
+        printf("\n %c %f %d ",b[i].name,b[i].price,b[i].pages);
+    return 0;
+}
