b025d8590ac8fb85404c146897ef5b1003e462a2
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-05-25 22:58:36 +0530

    Practic c

--- Structure/Nested structure.c
+++ Structure/Nested structure.c
@@ -1,1 +1,18 @@
 /* Nesting of Structure*/
+void main ()
+{
+    struct address
+    {
+        char phone[25];
+        char city [25];
+        int pin;
+    };
+    struct employee 
+    {
+        char name[25];
+        struct address a;
+    };
+    struct employee e={"Jeetu Pandey","9806638963","Satna",485001};
+    printf("\nName = %s  Phone = %s",e.name,e.a.phone);
+    printf("\nCity = %s  Pin = %d",e.a.city,e.a.pin);
+}
