9c644cc72e471e69bce58ecd4469996c87c4c6a2
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-05-25 23:19:10 +0530

    practic c

---
+++ Structure/1-Structure and Function.c
@@ -0,0 +1,16 @@
+/* Method- 01 Structure and Function*/
+void main()
+{
+    struct book
+    {
+        char name[25];
+        char author[25];
+        int pages;
+    };
+    struct book b1 = {"Learning C","SVP",750};
+    display(b1.name,b1.author.b1.pages);
+}
+display(char *s,char *t, int n)
+{
+    printf("\n%s  %s  %d",s,t,n);
+}
---
+++ Structure/2-Structure and Function.c
@@ -0,0 +1,16 @@
+/* Method- 02 Structure and Function*/
+struct book                 //Global Scope
+    {
+        char name[25];
+        char author[25];
+        int pages;
+    };
+void main()
+{
+    struct book b1 = {"Learning C","SVP",750};
+    display(b1);
+}
+display(struct book b)
+{
+    printf("\n%s  %s  %d",b.name,b.author,b.pages);
+}
