8ba76f6d92a246f3408b308ad7ed3ad8c190c426
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-05-26 21:45:15 +0530

    practic c

--- Union/union eg.c
+++ Union/union eg.c
@@ -1,1 +1,15 @@
-/*Union eg*/ 
+/* Union Example - Write a program To Understand the working Union*/ 
+#include<stdio.h>
+void main()
+{
+    union test
+    {
+        int a;
+        char b[2];
+    };
+    union test x;
+    x.a = 512;
+    printf("\n x.a = %d",x.a);
+    printf("\n x.b[0] = %d",x.b[0]);
+    printf("\n x.b[1] = %d",x.b[1]);
+}
