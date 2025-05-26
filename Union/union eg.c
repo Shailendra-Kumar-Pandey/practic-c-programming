3f378b58520dd4a1a8e31cf1196126e9282c5f0f
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-05-26 22:49:27 +0530

    Practic c

--- Union/union eg.c
+++ Union/union eg.c
@@ -1,4 +1,4 @@
-/* Union Example - Write a program To Understand the working Union*/ 
+/* Union Example - Write a program To Understand the working Union*/
 #include<stdio.h>
 void main()
 {
@@ -13,3 +13,15 @@ void main()
     printf("\n x.b[0] = %d",x.b[0]);
     printf("\n x.b[1] = %d",x.b[1]);
 }
+
+/* Now, let us understand this output in detail 512 is an integer a 2 byte number.
+Its binary equivalent will be 0000 0010 0000 0000 . we assume that this binary number
+when stored in memory would look as show below
+
+
+    our                         0000 0010 | 0000 0000
+assumption                      high byte   low byte
+
+
+actually stored                 0000 0000 | 0000 0010      //   {10 = 2} (convert binary to integer)
+in the memory                   low byte   high byte                      */
