9c755327bfb55fc9a7bb0954994b2010eb7b4266
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-05-29 21:32:24 +0530

    Practic c

--- File in C/file 1.c
+++ File in C/file 1.c
@@ -1,1 +1,22 @@
-/* Write a Program to  */
+/* Write a Program to Receives string from keyboard and writes them to File */
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void main()
+{
+    FILE *fp;
+    char s[80];
+    fp = fopen("DATA.TXT", "w");
+    if(fp == NULL)
+    {
+        puts("Cannot open File");
+        exit(1);
+    }
+    printf("\nEnter a few lines of text:\n");
+    while(strlen(gets(s))>0)
+    {
+        fputs(s,fp);
+        fputs("\n",fp);
+    }
+    fclose(fp);
+}
