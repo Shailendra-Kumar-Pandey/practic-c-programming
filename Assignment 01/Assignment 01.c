8482b94c51b023db7c8c194ee756e9750716c2f4
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-05-31 21:57:55 +0530

    practic c

---
+++ Assignment 01/Assignment 01.c
@@ -0,0 +1,12 @@
+/* Write a Program to following deatailes
+
+                        MENU
+    1) Addition of 3 Numbers
+    2) Multification of 3 Numbers
+    3) Average of 3 Numbers
+    4) Biggest out of 3 Numbers
+    5) Smallest out of 3 Numbers
+    6) Stop
+
+    Enter your choice (1-6):-
+*/
