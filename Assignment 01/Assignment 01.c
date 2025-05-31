f3b28fce9e5746e97ef96a4636dd2e15ba73b6ea
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-05-31 22:49:54 +0530

    Practic C

--- Assignment 01/Assignment 01.c
+++ Assignment 01/Assignment 01.c
@@ -1,8 +1,8 @@
-/* Write a Program to following deatailes
+/* Write a Program to following details
 
                         MENU
     1) Addition of 3 Numbers
-    2) Multification of 3 Numbers
+    2) Multiplication of 3 Numbers
     3) Average of 3 Numbers
     4) Biggest out of 3 Numbers
     5) Smallest out of 3 Numbers
@@ -10,3 +10,13 @@
 
     Enter your choice (1-6):-
 */
+#include<stdio.h>
+int main()
+{
+    int enterCode = 0;
+    do{
+        printf("\n  \t   MENU\n 1) Addition of 3 Numbers\n 2) Multiplication of 3 Numbers\n 3) Average of 3 Numbers\n 4) Biggest out of 3 Numbers\n 5) Smallest out of 3 Numbers\n 6) Stop\n \nEnter your choice (1-6):- ");
+        scanf("%d",&enterCode);
+    }while(enterCode != 6);
+    return 0;
+}
