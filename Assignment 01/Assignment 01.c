3ef7cb471ebf8b39fdb4d299d2513ddcc26cfdb2
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-05-31 23:51:03 +0530

    Practic c

--- Assignment 01/Assignment 01.c
+++ Assignment 01/Assignment 01.c
@@ -17,6 +17,70 @@ int main()
     do{
         printf("\n  \t   MENU\n 1) Addition of 3 Numbers\n 2) Multiplication of 3 Numbers\n 3) Average of 3 Numbers\n 4) Biggest out of 3 Numbers\n 5) Smallest out of 3 Numbers\n 6) Stop\n \nEnter your choice (1-6):- ");
         scanf("%d",&enterCode);
+        if(enterCode == 1)
+            addition();
+        else
+            if(enterCode == 2)
+                multiplication();
+            else
+                if(enterCode == 3)
+                    average();
+                else
+                    if(enterCode == 4)
+                        biggest();
+                    else
+                        if(enterCode == 5)
+                            smallest();
+                        else
+                            if(enterCode<1 || enterCode>6)
+                                printf("\n Number is Not Valid, Please Enter Valid Number \n");
     }while(enterCode != 6);
     return 0;
+}
+addition()
+{
+    int a, b, c, add;
+    printf("\n Enter the 3 Number :- ");
+    scanf("%d%d%d",&a,&b,&c);
+    add = a + b + c;
+    printf("\nAddition = %d \n",add);
+}
+multiplication()
+{
+    int a, b, c, multi;
+    printf("\n Enter the 3 Number :- ");
+    scanf("%d%d%d",&a,&b,&c);
+    multi = a * b * c;
+    printf("\nMultiplication = %d \n",multi);
+}
+average()
+{
+    int a, b, c, add;
+    printf("\n Enter the 3 Number :- ");
+    scanf("%d%d%d",&a,&b,&c);
+    add = a + b + c;
+    printf("\nAverage = %d \n",add);
+}
+biggest()
+{
+    int a, b, c, big;
+    printf("\n Enter the 3 Number :- ");
+    scanf("%d%d%d",&a,&b,&c);
+    big = a + b + c;
+    printf("\nBiggest = %d \n",big);
+}
+smallest()
+{
+    int a, b, c, small;
+    printf("\n Enter the 3 Number :- ");
+    scanf("%d%d%d",&a,&b,&c);
+    if( a < small)
+        small = a;
+    else
+        if( b < small)
+            small = b;
+        else
+            if(c < small)
+                small = c;
+    printf("\nSmallest = %d \n",small);
 }
