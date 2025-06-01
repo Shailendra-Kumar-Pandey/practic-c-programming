896ace8c6251ece8b79f70ce86a0faf126af9423
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-06-01 19:49:28 +0530

    Add 2nd ass. Question

---
+++ Assignment 02/Assignment 02.c
@@ -0,0 +1,21 @@
+  /* Write a Progream to following step:-
+    First Page - 01  
+             Ral English High School
+    1) Login.........
+    2) Register.........
+    3) Forget Login ID/Password........
+    4) Exit......
+
+    Enter your choice(1-4):-
+
+    Second Page - 02
+             Ral English High School
+    1) Enter Student Data (Roll No, Name, Gender, Marks of 5 subject)
+    2) Append New Data
+    3) Display Result Sheet
+    4) Display Merit List
+    5) Display List of Boys/Girls
+    6) Exit
+
+    Enter your choice(1-6):-
+  */
