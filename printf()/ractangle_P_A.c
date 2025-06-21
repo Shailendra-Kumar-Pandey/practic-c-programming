c450f8386545af04522530e17f9d6813011b5c3f
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-06-21 21:50:13 +0530

    Practic c

--- printf()/ractangle_P_A.c
+++ printf()/ractangle_P_A.c
@@ -6,7 +6,7 @@ int main()
     width = 55.0;
     area = lenght * width;
     perimeter = 2 * (lenght + width);
-    printf("\n Area of Ractangle = %f", area);
-    printf("\n Perimeter of Ractangle = %f", perimeter);
+    printf("\n Area of Rectangle = %f", area);
+    printf("\n Perimeter of Rectangle = %f", perimeter);
     return 0;
 }
--- scanf()/ractangle.c
+++ scanf()/ractangle.c
@@ -7,7 +7,7 @@ int main()
     printf("\n Enter the value of width :");
     scanf("%f",&width);
     area = lenght * width;
-    perimeter = 2 * (lenght * width);
+    perimeter = 2 * (lenght + width);
     printf("\n Area of Ractangle : = %f",area);
     printf("\n Perimeter of Ractangle : = %f",perimeter);
     return 0;
